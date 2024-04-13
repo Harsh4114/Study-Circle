e31b3084b523f9c387ef05505df5b844ded7d11c
Harsh <hkpatel747@gmail.com>
2024-04-13 18:17:36 +0530

    Update HomeUi.dart

--- lib/pages/HomeUi.dart
+++ lib/pages/HomeUi.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors, file_names, non_constant_identifier_names
+// ignore_for_file: prefer_const_constructors, file_names, non_constant_identifier_names, unused_local_variable, body_might_complete_normally_nullable
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:codeblock/Service/AUTHENTICATION/Authentication.dart';
 import 'package:codeblock/pages/loginpage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -13,44 +14,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int number = 0;
-  String Data = " ";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: IconButton(
-                onPressed: () {
-                  final String user =
-                      FirebaseAuth.instance.currentUser!.email.toString();
-                  FirebaseAuth.instance.signOut().then((value) {
-                    Authentication().SignOut(user);
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => Loginpage()));
-                  });
-                },
-                icon: Icon(Icons.logout_outlined)),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: IconButton(
+                    onPressed: () {
+                      final String? user =
+                          FirebaseAuth.instance.currentUser?.email;
+                      FirebaseAuth.instance.signOut().then((value) {
+                        Authentication().SignOut(user!);
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Loginpage()));
+                      });
+                    },
+                    icon: Icon(Icons.logout_outlined)),
+              ),
+              StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection("Posts")
+                      .orderBy("Time", descending: true)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.active) {
+                      if (snapshot.hasData) {
+                        return Expanded(
+                          child: ListView.builder(
+                            itemCount: snapshot.data!.docs.length,
+                            itemBuilder: (context, index) {
+                              Map<String, dynamic> data =
+                                  snapshot.data!.docs[index].data()
+                                      as Map<String, dynamic>;
+
+                              return Card(
+                                color: Colors.lightBlue[100],
+                                child: ListTile(
+                                  leading: Text(
+                                    data["Post"],
+                                    style: TextStyle(fontSize: 10),
+                                  ),
+                                  title: Text(data["Topic"]),
+                                  subtitle: Text(data["Description"]),
+                                  onLongPress: () {
+                                    snapshot.data!.docs[index].reference
+                                        .delete();
+                                  },
+                                  // trailing: Text(data["Post By"]),
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      } else {
+                        return Text("No Data Found");
+                      }
+                    } else {
+                      return CircularProgressIndicator();
+                    }
+                  }),
+            ],
           ),
-          Text("$number $Data"),
-        ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            number++;
-            if (number % 2 == 0) {
-              setState(() {
-                Data = "Is Even";
-              });
-            } else {
-              setState(() {
-                Data = "Is Odd";
-              });
-            }
+        onPressed: () async {
+          dynamic time = DateTime.now();
+          String user =
+              FirebaseAuth.instance.currentUser!.displayName.toString();
+          await FirebaseFirestore.instance.collection("Posts").add({
+            "Topic": "Try",
+            "Description": "Flutter is a UI toolkit",
+            "Time": time,
+            "Post": user,
           });
+          print("Data Added");
         },
         hoverColor: Colors.blue,
         child: Icon(Icons.add),
